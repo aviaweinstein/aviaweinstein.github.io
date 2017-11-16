879c6873e4d1c6c415ec567d4c3f44bbd1d6218b
Avia Weinstein <aviaweinstein@Avias-MacBook-Air.local>
2017-11-16 02:29:28 -0500

    fixes dag-move bug with scroll
    when on touchscreen, there is no clean way to scroll if dragable image takes up entire screen. This added functionality makes it so that you have to touch the screen with 2 or more fingers in order to drag so that 1 finger touching still accomplishes the expected page scroll

--- assets/javascript/drag_move.js
+++ assets/javascript/drag_move.js
@@ -3,7 +3,16 @@ $(document).ready(function(){
   var frame = 0;
 
   $('.move').on('mousedown touchstart', function(e) {
-    e.preventDefault();
+    if (e.touches) {
+      if (e.touches.length > 1) {
+        e.preventDefault();
+      } else {
+        $('.move').css('opacity', 0.5);
+        //TODO: add text about needign at least 2 fingers to interact
+      }
+    } else {
+      e.preventDefault();
+    }
     var newY = null;
     var oldY = null;
 
@@ -23,15 +32,43 @@ $(document).ready(function(){
       changeFrame(e, 2);
     });
 
-    $('.move').on('touchmove',function(e) {
-      e.preventDefault();
-      changeFrame(e, 5);
-    });
+    if (e.touches && e.touches.length > 1) {
+      $('.move').on('touchmove',function(e) {
+        e.preventDefault();
+        changeFrame(e, 5);
+      });
+    }
 
-    $('.move').on('mouseup touchend', function(e) {
-      e.preventDefault();
+    $('.move').on('mouseend touchend', function(e) {
+      if (e.touches) {
+        if (e.touches.length > 1) {
+          e.preventDefault();
+        } else {
+          $('.move').css('opacity', 1);
+          //TODO: remove text about needign at least 2 fingers to interact
+        }
+      } else {
+        e.preventDefault();
+      }
       $('.move').unbind('mousemove touchmove');
     });
+    //if (e.touches) {
+    //  if (e.touches.length > 1) {
+    //    $('.move').on('touchend', function(e) {
+    //      e.preventDefault();
+    //      $('.move').unbind('touchmove');
+    //    });
+    //  } else {
+    //    $('.move').on('touchend', function(e) {
+    //      $('.move').css('opacity', 1);
+    //    });
+    //  }
+    //} else {
+    //  $('.move').on('mouseend', function(e) {
+    //      e.preventDefault();
+    //      $('.move').unbind('mousemove');
+    //    });
+    //}
   });
   
 });
